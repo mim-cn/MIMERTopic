0992c482757f463c2e98490bbb0374c4d7bac701
jk19910410 <wangkai@192.168.1.9>
2018-05-02 21:33:29 +0800

    Initial Commit

---
+++ MIMTopic/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MIMTopic
+//
+//  Created by wangkai on 2018/5/2.
+//  Copyright © 2018年 wangkai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
